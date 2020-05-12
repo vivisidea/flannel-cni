b535147c24a1b3948c1ec58459e6ca71409b7ef3
yangweiqiang <yangweiqiang@corp.netease.com>
2020-05-12 20:38:16 +0800

    upgrade flannel-cni plugin to v0.7.0

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -2,12 +2,12 @@
 set -e
 CURL="curl -sSf"
 FLANNEL_CNI_ROOT=$(git rev-parse --show-toplevel)
-IMAGE_NAME=quay.io/coreos/flannel-cni
-VERSION=$($FLANNEL_CNI_ROOT/scripts/git-version)
-CNI_VERSION="v0.6.0"
+IMAGE_NAME=private-registry.yidun.internal/coreos/flannel-cni
+VERSION=$($FLANNEL_CNI_ROOT/scripts/git-version)-fix1
+CNI_VERSION="v0.7.0"
 
 mkdir -p dist
-$CURL -L --retry 5 https://github.com/containernetworking/cni/releases/download/$CNI_VERSION/cni-amd64-$CNI_VERSION.tgz | tar -xz -C dist/
+#$CURL -L --retry 5 https://github.com/containernetworking/cni/releases/download/$CNI_VERSION/cni-amd64-$CNI_VERSION.tgz | tar -xz -C dist/
 $CURL -L --retry 5 https://github.com/containernetworking/plugins/releases/download/$CNI_VERSION/cni-plugins-amd64-$CNI_VERSION.tgz | tar -xz -C dist/
 
 docker build --no-cache -t $IMAGE_NAME:$VERSION .
